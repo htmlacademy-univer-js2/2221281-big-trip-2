501061950e24f0de578a49e872b26163812d2b3f
j0k3em <dmitry.lyampasov@yandex.ru>
2023-06-09 16:12:27 +0500

    разделяй и властвуй

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import FiltersView from './view/filter-view';
 import MenuView from './view/menu-navigation-view';
 import EventsPresenter from './presenter/presenter';
-import { render } from './render';
+import { render } from './framework/render';
 import PointsModel from './model/points-model';
 import { getPoints, getDestinations, getOffersByType } from './mock/point';
 
