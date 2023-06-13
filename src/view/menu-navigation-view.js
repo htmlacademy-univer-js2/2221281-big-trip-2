93f953c588d109d44415caff01a81af871a48427
j0k3em <dmitry.lyampasov@yandex.ru>
2023-06-13 12:21:04 +0500

    меняй-удаляй

--- src/view/menu-navigation-view.js
+++ src/view/menu-navigation-view.js
@@ -2,8 +2,7 @@ import AbstractView from '../framework/view/abstract-view';
 
 const menuTemplate = () => (`<nav class="trip-controls__trip-tabs  trip-tabs">
 <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
-<a class="trip-tabs__btn" href="#">Stats</a>
-</nav>`)
+</nav>`);
 
 export default class MenuView extends AbstractView{
   get template() {
